898d059e0c7600bc033430bed4e14682c643fe7d
jirwincline <jakobinwins@gmail.com>
2019-07-19 13:19:27 -0700

    add logic for checking if the word has a vowel

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,11 +3,16 @@ require 'fileutils'
 
 class Anagram
   def initialize(str1, str2)
-    @arr1 = str1.downcase.split('').sort.delete_if{ |x| x == " "}
-    @arr2 = str2.downcase.split('').sort.delete_if{ |x| x == " "}
-    @string = str1
-    p @arr1
-    p @arr2
+      @arr1 = str1.downcase.split('').sort.delete_if{ |x| x == " "}
+      @arr2 = str2.downcase.split('').sort.delete_if{ |x| x == " "}
+      if @arr1.any?{|x| ["a","e","i","o","u"].include?(x)} || @arr2.any?{ |y| ["a","e","i","o","u"].include?(y)}
+      @string = str1
+      p @arr1
+      p @arr2
+    else
+        p "please enter real words"
+        "please enter real words"
+    end
   end
   def anagrams?
     count = 0
@@ -17,14 +22,23 @@ class Anagram
       end
     end
     if count == 0
-      "these are ANTIgrams"
+      p "these are ANTIgrams"
+      false
     else
       if @arr1 == @arr2
-        "these are anagrams!"
-
+        p "these are anagrams!"
+        true
       else
-        "These are not anagrams"
+        p "These are not anagrams"
+        false
       end
     end
   end
 end
+
+p "please give us your strings"
+str1 = gets.chomp
+p "and"
+str2 = gets.chomp
+strings = Anagram.new(str1,str2)
+strings.anagrams?
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -11,4 +11,9 @@ describe('#anagrams') do
     anagram = Anagram.new('God', 'dog')
      expect(anagram.anagrams?).to(eq("these are anagrams!"))
   end
+  it('if two strings entered are anagrams, but have different capital letters, they should still return true') do
+    anagram = Anagram.new('God', 'dog')
+     expect(anagram.anagrams?).to(eq("these are anagrams!"))
+  end
+
 end
