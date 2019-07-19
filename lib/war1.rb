bf02b84a56514b025818c46488979bdd8ae44051
jirwincline <jakobinwins@gmail.com>
2019-07-19 10:30:12 -0700

    create files for project

---
+++ lib/war1.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require 'fileutils'
+class Class_name
+  def initialize()
+
+  end
+  def method
+
+  end
+end
---
+++ spec/war1_spec.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'war1.rb'
+
+describe('war1') do
+  it('this is your generic test') do
+
+  end
+end
