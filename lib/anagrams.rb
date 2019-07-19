c88ad558fa877fefb3e683008779b68abbc01e3d
jirwincline <jakobinwins@gmail.com>
2019-07-19 11:23:06 -0700

    added a class for anagrams and stored both strings as arrays

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,11 +1,20 @@
 require 'pry'
 require 'fileutils'
 
-class Class_name
-  def initialize()
-
+class Anagram
+  def initialize(str1, str2)
+    @arr1 = str1.split('')
+    @arr2 = str2.split('')
+    p @arr1
+    p @arr2
   end
-  def method
+  def anagrams?
 
   end
 end
+
+p "please give us your strings"
+str1 = gets.chomp
+p "and"
+str2 = gets.chomp
+strings = Anagram.new(str1,str2)
