b55c0fb0319ceb906c59883f88f2d918ae36e333
jirwincline <jakobinwins@gmail.com>
2019-07-19 13:29:32 -0700

    write tests further

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -11,9 +11,14 @@ describe('#anagrams') do
     anagram = Anagram.new('God', 'dog')
      expect(anagram.anagrams?).to(eq("these are anagrams!"))
   end
-  it('if two strings entered are anagrams, but have different capital letters, they should still return true') do
-    anagram = Anagram.new('God', 'dog')
-     expect(anagram.anagrams?).to(eq("these are anagrams!"))
+  it('if one of the words doesnt contain a vowel, will output a string that asks for real words') do
+    anagram = Anagram.new('x', 'y')
+    expect(anagram.anagrams?).to(eq("please enter real words"))
   end
 
+  it('if two strings entered are not anagrams, and have NO matching letters otput a string that lets them know they are antigrams') do
+    anagram = Anagram.new('God', 'dog')
+     STDOUT.should_receive(:p).with("these are ANTIgrams")
+     expect(anagram.anagrams?).to(eq(false))
+  end
 end
