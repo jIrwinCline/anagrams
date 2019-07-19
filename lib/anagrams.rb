b2191c6f2c61031ee87864bbbc8d8c2a07910b7f
jirwincline <jakobinwins@gmail.com>
2019-07-19 12:18:12 -0700

    add logic to find antigrams and return a strring stating so

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -5,15 +5,26 @@ class Anagram
   def initialize(str1, str2)
     @arr1 = str1.downcase.split('').sort.delete_if{ |x| x == " "}
     @arr2 = str2.downcase.split('').sort.delete_if{ |x| x == " "}
+    @string = str1
     p @arr1
     p @arr2
   end
   def anagrams?
-    if @arr1 == @arr2
-      true
-
+    count = 0
+    @arr2.each do |i|
+      if @string.include?(i)
+        count += 1
+      end
+    end
+    if count == 0
+      "these are ANTIgrams"
     else
-      false
+      if @arr1 == @arr2
+        "these are anagrams!"
+
+      else
+        "These are not anagrams"
+      end
     end
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,10 +5,10 @@ require 'anagrams.rb'
 describe('#anagrams') do
   it('if two strings entered are anagrams, they should return true') do
     anagram = Anagram.new('dot', 'tod')
-     expect(anagram.anagrams?).to(eq(true))
+     expect(anagram.anagrams?).to(eq("these are anagrams!"))
   end
   it('if two strings entered are anagrams, but have different capital letters, they should still return true') do
     anagram = Anagram.new('God', 'dog')
-     expect(anagram.anagrams?).to(eq(true))
+     expect(anagram.anagrams?).to(eq("these are anagrams!"))
   end
 end
